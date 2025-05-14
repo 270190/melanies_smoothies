ac03983d5cc29d3be1f30a620951c787d423e478
270190 <50192692+270190@users.noreply.github.com>
2025-05-14 15:44:16 +0700

    Update streamlit_app.py
    To modify Api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ if ingredietns_list:
         session.sql(my_insert_stmt).collect()
 
         st.success("Your Smoothie is ordered," + name_on_order +"!", icon="✅")
+
+# New session to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
